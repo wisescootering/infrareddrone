df5a5b451662a01ada119a2e2e3511ef527854e2
Balthazar <wisescootering@gmail.com>
2021-09-26 18:21:31 +0200

    [ALIGNMENT] Quadratic fit for Hessian and Gradients on a 3x3 neighborhood based on HDR+ paper. Switch to gradient descent when required.

--- registration/newton.py
+++ registration/newton.py
@@ -50,8 +50,8 @@ So it could be easier to retrieve Hessian A and gradient Ax+B
 """
 import numpy as np
 import logging
-from registration.constants import NEWTON, GRADIENT_DESCENT
-
+from registration.constants import NEWTON, GRADIENT_DESCENT, QUADRATIC_FORM, SOBEL_DERIVATIVES
+from scipy.ndimage import convolve
 
 def classic_gradient(cost, sobel_flag=False):
     """
@@ -62,11 +62,11 @@ def classic_gradient(cost, sobel_flag=False):
     sobel_ = cost[:, 2:, :] - cost[:, :-2, :]
     grad_y = sobel_[1:-1]
     if sobel_flag:  # REMOVE FOR NUMBA?
-        grad_y = sobel_[:-2] + sobel_[2:] + 2*sobel_[1:-1]
+        grad_y = (sobel_[:-2] + sobel_[2:] + 2*sobel_[1:-1])/4.
     sobel_ = cost[2:, :, :] - cost[:-2, :, :]
     grad_x = sobel_[:, 1:-1, :]
     if sobel_flag:  # REMOVE FOR NUMBA?
-        grad_x = sobel_[:, :-2] + sobel_[:, 2:] + 2*sobel_[:, 1:-1]
+        grad_x = (sobel_[:, :-2] + sobel_[:, 2:] + 2*sobel_[:, 1:-1])/4.
     grads[:, :, :, 0] = grad_y / 2.
     grads[:, :, :, 1] = grad_x / 2.
     return grads
@@ -90,6 +90,7 @@ def hessian(grad_x_y, sobel_flag=False):
 
 def derivatives(cost, sobel_flag=False):
     """
+    Naive gradient computation using the Sobel filter ... and the hessian as the matrix of Sobel of Sobel.
     2N+1, 2M+1, C 
     -> gradient (2N-1 , 2M-1, C, 2)
     -> hessian  (2N-3 , 2M-3, C, 2, 2)
@@ -100,32 +101,162 @@ def derivatives(cost, sobel_flag=False):
     """
     grads = classic_gradient(cost, sobel_flag=sobel_flag)
     hessi = hessian(grads, sobel_flag=sobel_flag)
-    return grads, hessi
+    constant = cost
+    return hessi, grads, constant
 
 
-def newton_iter(previous_val, grad_vec, hess_mat=None, alpha=1.):
-    step = np.zeros_like(previous_val) # (ch, 2)  ch, [x, y]
+def quadric_approximation(full_cost):
+    """
+    -> gradient (2N-1 , 2M-1, C, 2)
+    -> hessian  (2N-1 , 2M-1, C, 2, 2)
+    Based on the HDR+ paper (supplemental material)
+    http://www.hdrplusdata.org/hdrplus_supp.pdf   section 4
+    Requires a 3x3 neighborhood
+
+    z = 1/2 . u^T . A. u + B u + C
+    B = [b1, b2]
+    A = [a11 a12]
+        [a21 a22]
+
+    z is a quadric form to approximate cost function y.
+
+    b1 & b2 are the gradients.. and are exactly Sobel filters
+         [ -1   0   1 ]
+    b1 = [ -2   0   2 ] /8 * y
+         [ -1   0   1 ]
+
+         [ -1  -2  -1 ]
+    b2 = [  0   0   0 ] /4 * y
+         [  1   2   1 ]
+
+
+          [1   -2   1]
+    a11 = [2   -4   2] /4  * y
+          [1   -2   1]
+
+          [ 1   2   1]
+    a22 = [-2  -4  -2] /4  * y
+          [ 1   2   1]
+
+                [ 1  0 -1 ]
+    a12 = a21 = [ 0  0  0 ] /4 * y
+                [-1  0  1 ]
+
+        [-1  2  -1]
+    C = [2  12   2] /16 * y
+        [-1  2  -1]
+    """
+    if full_cost.shape[0] !=3 or full_cost.shape[1]!=3:
+        full_cost = full_cost[
+            full_cost.shape[0]//2 - 1:full_cost.shape[0]//2 + 2,
+            full_cost.shape[1]//2 - 1:full_cost.shape[1]//2 + 2,
+            :
+        ]
+    grad_x_conv = np.array([
+        [-1., 0., 1.],
+        [-2., 0., 2.],
+        [-1., 0., 1.],
+    ])/8. # b1
+    grad_y_conv = grad_x_conv.T  # b2
+    hess_xx_conv = np.array([
+        [1., -2., 1.],
+        [2., -4., 2.],
+        [1., -2., 1.],
+    ])/4.
+    hess_yy_conv = hess_xx_conv.T
+    hess_xy_conv = np.array([
+        [ 1, 0, -1 ],
+        [ 0, 0,  0 ],
+        [-1, 0,  1 ]
+    ])/4.
+    constant_conv = np.array([
+        [-1,  2, -1 ],
+        [ 2, 12,  2 ],
+        [-1,  2, -1 ]
+    ])/16.
+    n_channels = full_cost.shape[-1]
+    hessi = np.empty((n_channels, 2, 2))
+    grads = np.empty((n_channels, 2))
+    constants = np.empty((n_channels))
+    for ch in range(n_channels):
+        cost = full_cost[:, :, ch]
+        hess_xx = np.sum(cost * hess_xx_conv)
+        hess_yy = np.sum(cost * hess_yy_conv)
+        hess_xy = np.sum(cost * hess_xy_conv)
+        grad_x = np.sum(cost * grad_x_conv)
+        grad_y = np.sum(cost * grad_y_conv)
+        constants[ch] = np.sum(cost * constant_conv)
+        hessi[ch, :, :] = np.array([
+            [hess_xx, hess_xy],
+            [hess_xy, hess_yy]
+        ])
+        grads[ch, :] = np.array([grad_x, grad_y])
+    return hessi, grads, constants
+
+
+def newton_iter(previous_val, grad_vec, hess_mat=None, alpha=1., max_step=1.):
+    step = np.zeros_like(previous_val)
     n_ch = grad_vec.shape[0]
     for ch in range(n_ch):
         new_val = previous_val
         if hess_mat is None:
             step += - alpha*grad_vec[ch, :]
         else:
-            step += - alpha*np.dot(np.linalg.inv(hess_mat[ch, :, :]), grad_vec[ch, :])
+            try:
+                hess_inv = np.linalg.inv(hess_mat[ch, :, :])
+                current_step = alpha*np.dot(hess_inv, grad_vec[ch, :])
+                current_step_norm = np.sqrt(np.sum(current_step**2))
+                if max_step is not None and current_step_norm > max_step:
+                    logging.info("TRUNCATE DISPLACEMENT TO MAXIMUM {} pixel".format(max_step))
+                    current_step = current_step/(current_step_norm/max_step)
+                logging.info("Channel {}\tcurrent step {}".format(ch, current_step))
+
+                step += - alpha*current_step
+            except:
+                current_step = grad_vec[ch, :]/np.sqrt(np.sum(grad_vec[ch, :]**2))
+                logging.warning("CANNOT INVERT HESSIAN MATRIX! FALLBACK TO GRADIENT DESCENT!\nChannel {}\tcurrent step {}".format(ch, current_step))
+                step += - current_step #MOVE BY ONE PIXEL ALONG THE SLOPE!
+                # print("STEP: {} - grad {}".format(step, grad_vec[ch, :]))
+
     new_val += step/n_ch
     return new_val
 
 
-def get_derivatives_at_position(cost, x_pos, y_pos):
+def plane_approximation(cost_full):
+    n_channels = cost_full.shape[0]
+    hessians = None
+    # hessians = np.empty((n_channels, 2, 2))
+    grads = np.empty((n_channels, 2))
+    constants = np.empty((n_channels, 1))
+    for ch in range(n_channels):
+        cost = cost_full[ch, :, :]
+        size_y, size_x = cost.shape[0], cost.shape[1]
+        a_matrix = np.empty((size_x*size_y, 3))
+        center_y, center_x = cost.shape[0]//2, cost.shape[1]//2
+        for id_y in range(size_y):
+            for id_x in range(size_x):
+                a_matrix[id_y*size_x+id_x, :] = np.array([id_x-center_x, id_y-center_y, 1.])
+        plane = np.linalg.lstsq(a_matrix, cost.flatten(), rcond=None)[0]
+        logging.info("PLANE APPROXIMATION {} - {} {}".format(plane, size_y, size_x))
+        grads[ch, :] = np.array([plane[0], plane[1]])
+        constants[ch, 0] = plane[2]
+    return hessians, grads, constants
+
+
+def get_derivatives_at_position(cost, x_pos, y_pos, mode=QUADRATIC_FORM):
+    debug_dict = None
     center_y, center_x = cost.shape[0]//2, cost.shape[1]//2
     id_y_f = y_pos + center_y
     id_x_f = x_pos + center_x
     id_x = int(np.round(id_x_f))
     id_y = int(np.round(id_y_f))
-    print("x ", x_pos, "y ", y_pos, " -- quantified position", id_x, id_y)
+    x_pos_discrete, y_pos_discrete = id_x - center_x, id_y - center_y
+    subpix_x = id_x_f - id_x
+    subpix_y = id_y_f - id_y
+    logging.info("Position - x {} y {}  [quantified x {} y {}]".format(x_pos, y_pos, id_x, id_y))
     if id_x < 3 or id_x > cost.shape[1]-3 or id_y < 3 or id_y > cost.shape[0]-3:
         logging.warning("WENT TOO FAR!")
-        return False, None, None, None, x_pos, y_pos
+        return False, None, None, None, x_pos_discrete, y_pos_discrete, debug_dict
 
     neighborhood_size = 2 # 5x5
     extracted_patch = cost[
@@ -133,14 +264,56 @@ def get_derivatives_at_position(cost, x_pos, y_pos):
         id_x-neighborhood_size:id_x+neighborhood_size+1,
         :
     ]
-    gradients, hess = derivatives(extracted_patch)
-    grads = gradients[1, 1, :, :]
-    hess_mat = hess[0, 0, :, :, :]
-    x_pos_discrete, y_pos_discrete = id_x - center_x, id_y - center_y
-    return True, extracted_patch, grads, hess_mat, x_pos_discrete, y_pos_discrete
+    if mode == SOBEL_DERIVATIVES:
+        hess, gradients, constants = derivatives(extracted_patch, sobel_flag=True)
+        hess_mat = hess[0, 0, :, :, :]
+        grads = gradients[1, 1, :, :]
+        constants = extracted_patch[2, 2, :]
+    elif mode == QUADRATIC_FORM:
+        hess_mat, grads, constants = quadric_approximation(extracted_patch)
+    hess_mat[:, 0, 0] = hess_mat[:, 0, 0].clip(0, None)
+    hess_mat[:, 1, 1] = hess_mat[:, 1, 1].clip(0, None)
+    dets = hess_mat[:, 0, 0]  * hess_mat[:, 1, 1] - hess_mat[:, 0, 1]**2
+    hess_mat_original = hess_mat.copy()
+    grads_original = grads.copy()
+    constants_original = constants.copy()
+    plan_approx = False
+    for ch in range(hess_mat.shape[0]):
+        if dets[ch] <0:
+            logging.warning("NON SEMI-POSITIVE MATRIX! \n {} , determinant = {}".format(hess_mat[ch, :,: ], dets[ch]))
+            hess_mat[ch, 1, 0] = 0.
+            hess_mat[ch, 0, 1] = 0.
+        if hess_mat[ch, 0, 0] == 0. or hess_mat[ch, 1, 1] == 0.:
+            plan_approx = True
+            logging.warning("NON INVERTIBLE HESSIAN! {}".format(hess_mat[ch, :, :]))
+            neighborhood_size = 1 # 3x3
+            extracted_patch_small = cost[
+                  id_y-neighborhood_size:id_y+neighborhood_size+1,
+                  id_x-neighborhood_size:id_x+neighborhood_size+1,
+                  :
+              ]
+            _, grads_plane, _constant = plane_approximation(np.array([extracted_patch_small[:, :, ch]]))
+            logging.info("GRADS {} VS PLANE FITITNG {}".format(grads[ch, :], grads_plane))
+            grads[ch, :] = grads_plane
+            constants[ch] = _constant
+            hess_mat[ch, :, :] = 0.
+    shifted_grad = np.dot(hess_mat, np.array([subpix_x, subpix_y]).T)
+    debug_dict = dict(
+        cost=cost,
+        grad=grads,
+        hess=hess_mat,
+        constants=constants,
+        offset=[x_pos_discrete, y_pos_discrete]
+    )
+    if plan_approx:
+        debug_dict["hess_original"] = hess_mat_original
+        debug_dict["grad_original"] = grads_original
+        debug_dict["constants_original"] = constants_original
+    grads_final = grads+shifted_grad
+    return True, extracted_patch, grads_final, hess_mat, x_pos_discrete, y_pos_discrete, debug_dict
 
 
-def search_minimum_full_patch_discrete(cost, init_val=None, iter=20, alpha=0.5, mode=[NEWTON, GRADIENT_DESCENT][0]):
+def search_minimum_full_patch_discrete(cost, init_val=None, iter=20, alpha=0.5, mode=[NEWTON, GRADIENT_DESCENT][0], max_step=1.):
     if init_val is None:
         new_val = np.array([0., 0.]).T
     else:
@@ -148,22 +321,41 @@ def search_minimum_full_patch_discrete(cost, init_val=None, iter=20, alpha=0.5, 
     new_val_x_list = []
     new_val_y_list = []
     for i in range(iter):
-        ret, _selected_patch, grads, hess_mat,  x_pos_discrete, y_pos_discrete = get_derivatives_at_position(
+        ret, _selected_patch, grads, hess_mat,  x_pos_discrete, y_pos_discrete, dbg_dict = get_derivatives_at_position(
             cost,
             new_val[0],
             new_val[1]
         )
         if not ret:
-            print("EARLY STOP iteration {}".format(i))
+            logging.warning("EARLY STOP iteration {} - getting out of bounds".format(i))
+            new_val_x_list.append(new_val[0])
+            new_val_y_list.append(new_val[1])
+
             break
+
         new_val_x_list.append(new_val[0])
         new_val_y_list.append(new_val[1])
+        previous_val = new_val.copy()
         new_val = newton_iter(
-            np.array([x_pos_discrete, y_pos_discrete]).T.astype(np.float32),
+            np.array([new_val[0], new_val[1]]).T.astype(np.float32),
             grads,
             hess_mat = hess_mat if mode == NEWTON else None,
-            alpha=alpha
+            alpha=alpha,
+            max_step=max_step
         )
+        step_norm = np.sqrt((new_val-previous_val)**2).sum()
+        # FOLLOWING CODE IS FOR DEBUGGING THE CASES OF QUADRATIC FORM NOT BEING CORRECT APPROXIMATIONS OF THE SURFACE
+        # WORKAROUND IS TO SWITCH TO PLANAR FITTING, SET HESSIAN TO ZERO AND STICK TO A STEP OF GRADIENT DESCENT
+        # if (hess_mat[:, 0 , 0] == 0.).any() or (hess_mat[:, 1, 1] == 0.).any():
+        #     from registration.utlities import quadratic_approximation_plot
+        #     quadratic_approximation_plot(**dbg_dict, previous_position=previous_val, next_position=new_val)
+        # if np.fabs(new_val[0])>cost.shape[0]//2-2 or  np.fabs(new_val[1])>cost.shape[1]//2-2: #GETTING OUT OF BOUNDS!
+        #     from registration.utlities import quadratic_approximation_plot
+        #     quadratic_approximation_plot(**dbg_dict, previous_position=previous_val, next_position=new_val)
+        if step_norm<0.1:
+            logging.info("Finished converging! {}".format(step_norm))
+            break
+
     new_val_x_list = np.array(new_val_x_list)
     new_val_y_list = np.array(new_val_y_list)
     return cost, new_val_x_list, new_val_y_list
--- registration/utlities.py
+++ registration/utlities.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import os.path as osp
-import irdrone.utils as ut
 import irdrone.process as pr
 import numpy as np
-from registration.newton import search_minimum_full_patch_discrete, derivatives, newton_iter
-from registration.constants import NEWTON, GRADIENT_DESCENT
+from registration.newton import search_minimum_full_patch_discrete, derivatives, newton_iter, quadric_approximation
+from registration.constants import NEWTON, GRADIENT_DESCENT, QUADRATIC_FORM, SOBEL_DERIVATIVES
+import logging
 
 
 def plot_trajectory(x, y, z=None, ax=None):
     n_channels = len(x)
     if z is not None:
-        ax.plot(x[0], y[0], z[0], "r+", markersize=5)
         ax.plot(x[-1], y[-1], z[-1], "y+", markersize=5)
+        ax.plot(x[0], y[0], z[0], "r+", markersize=5)
         for i in range(len(x)-1):
             ax.plot(x[i:i+2], y[i:i+2], z[i:i+2], color=plt.cm.jet(int(255*i/n_channels)), markersize=5)
     else:
@@ -21,24 +21,32 @@ def plot_trajectory(x, y, z=None, ax=None):
                 x[i:i+2],
                 y[i:i+2],
                 "-o",
-                color = plt.cm.jet(int(255*3*i/N)%255),
+                color = plt.cm.jet(int(255*3*i/n_channels)%255),
                 markersize=1
             )
-        ax.plot(x[0], y[0], "r+", markersize=5)
         ax.plot(x[-1], y[-1], "y+", markersize=5)
+        ax.plot(x[0], y[0], "r+", markersize=5)
 
 
-def generate_synthetic_surface(size=5, center=[0.8, 0.9], ch_number=1):
+def generate_synthetic_surface(size=5, center=[0.8, 0.9], ch_number=1, grad=[0., 0.]):
     """Synthetic quadric surface, same function for all channels
     """
     single_cost = np.zeros((size, size, ch_number))
     surf_center_y, surf_center_x = single_cost.shape[0]//2, single_cost.shape[1]//2
     for y in range(single_cost.shape[0]):
         for x in range(single_cost.shape[1]):
-            single_cost[y, x, :] = 3*(x - (surf_center_x + center[0]))**2 + 1.*(y - (surf_center_y+center[1]))**2
-            # single_cost[y, x, 1] = 100.*np.sin(x *0.25)
-            # single_cost[y, x, :] += 0.1*x + 0.3*y + 0.1*x*y
-            # single_cost[y, x, :] += 100.*np.sin(x *0.25)
+            coord = np.array([x - (surf_center_x + center[0]), y - (surf_center_y+center[1])])
+            sx, sy = 3., 1.
+            theta = np.deg2rad(32.)
+            Q = np.array([[sx, 0.], [0., sy]])
+            Q = np.dot(Q, np.array(
+                [
+                    [np.cos(theta), np.sin(theta)],
+                    [-np.sin(theta), np.cos(theta)]
+                ])
+            )
+            single_cost[y, x, :] = np.dot(coord.T, np.dot(Q, coord))
+            single_cost[y, x, :]+= grad[0]*x + grad[1]*y
     return single_cost
 
 
@@ -72,45 +80,153 @@ def plot_search(cost, val_x, val_y):
         # ax.view_init(azim=90, elev=90)
         ax.view_init(azim=90, elev=30)
         ax = fig.add_subplot(n_channels, 2, ch*2+2)
-        plt.imshow(cost[:, :, ch], extent=[-cost.shape[0]//2 , cost.shape[0]//2, cost.shape[1]//2,  -cost.shape[1]//2])
+        plt.imshow(cost[:, :, ch], extent=[-cost.shape[0]//2 +0.5 , cost.shape[0]//2 +0.5, cost.shape[1]//2 +0.5,  -cost.shape[1]//2+0.5])
         plt.contour(coord_x, coord_y, cost[:, :, ch], 10,  cmap='RdGy', alpha=0.5)
         plot_trajectory(val_x, val_y, ax=ax)
     plt.tight_layout()
     plt.show()
 
 
-def search_minimum(single_cost, debug=False):
-    gradients, hess = derivatives(single_cost)
+def quadric_surface(coord, hess, grad, constant=None):
+    """
+    Compute x^TAx + bx + c
+    """
+    coord_z = np.dot(hess, coord)
+    coord_z = 0.5*(coord*coord_z).sum(axis=0)
+    coord_z += np.dot(grad, coord)
+    if constant is not None:
+        coord_z += constant
+    return coord_z
+
+
+def quadratic_approximation_plot(
+        cost=None, grad=None, hess=None, constants=None, offset=None, previous_position=None, next_position=None,
+        hess_original=None, grad_original=None, constants_original=None
+    ):
+    fig = plt.figure(figsize=plt.figaspect(1.))
+    n_channels = cost.shape[-1]
+    for ch in range(n_channels):
+        ax = fig.add_subplot(n_channels, 1, ch+1, projection='3d')
+        coord_y = np.arange(-cost.shape[0]//2+1, cost.shape[0]//2+1, 1.)
+        coord_x = np.arange(-cost.shape[1]//2+1, cost.shape[1]//2+1, 1.)
+        coord_x, coord_y = np.meshgrid(coord_x, coord_y)
+        _surf = ax.plot_wireframe(
+            coord_x, coord_y, cost[:, :, ch],
+            cmap=cm.viridis,
+            linewidth=1, antialiased=False, alpha=0.5
+        )
+        coord_y = np.linspace(-2, 2, num=20, endpoint=True)
+        coord_x = np.linspace(-2, 2, num=20, endpoint=True)
+        coord_x, coord_y = np.meshgrid(coord_x, coord_y)
+        coord = np.array([coord_x.flatten(), coord_y.flatten()])
+        coord_z = quadric_surface(coord, hess[ch, :, :], grad[ch, :], constant=cost[cost.shape[0]//2, cost.shape[1]//2, ch] if constants is None else constants[ch])
+        coord_z = coord_z.reshape(coord_x.shape)
+        if offset is not None:
+            coord_x += offset[0]
+            coord_y += offset[1]
+        _surf = ax.plot_surface(
+            coord_x, coord_y, coord_z,
+            cmap=cm.coolwarm,
+            linewidth=5, antialiased=False, alpha=0.5
+        )
+        if previous_position is not None and next_position is not None:
+            ax.plot(
+                [previous_position[0], next_position[0]],
+                [previous_position[1], next_position[1]],
+                [
+                    quadric_surface(np.array([previous_position[0] -offset[0], previous_position[1] -offset[1]]), hess[ch, :, :], grad[ch, :], constant=cost[cost.shape[0]//2, cost.shape[1]//2, ch] if constants is None else constants[ch]),
+                    quadric_surface(np.array([next_position[0] -offset[0], next_position[1] -offset[1]]), hess[ch, :, :], grad[ch, :], constant=cost[cost.shape[0]//2, cost.shape[1]//2, ch] if constants is None else constants[ch]),
+                ],
+                "k-o", markersize=1
+            )
+        if previous_position is not None:
+            ax.plot(
+                previous_position[0],
+                previous_position[1],
+                quadric_surface(np.array([previous_position[0]-offset[0], previous_position[1]-offset[1]]).T, hess[ch, :, :], grad[ch, :], constant=cost[cost.shape[0]//2, cost.shape[1]//2, ch] if constants is None else constants[ch]),
+                "r+", markersize=5)
+        if next_position is not None:
+            ax.plot(
+                next_position[0],
+                next_position[1],
+                quadric_surface(np.array([next_position[0]-offset[0], next_position[1]-offset[1]]).T, hess[ch, :, :], grad[ch, :], constant=cost[cost.shape[0]//2, cost.shape[1]//2, ch] if constants is None else constants[ch]),
+                "y+", markersize=5)
+        if grad_original is not None and hess_original is not None:
+            coord_y = np.linspace(-2, 2, num=20, endpoint=True)
+            coord_x = np.linspace(-2, 2, num=20, endpoint=True)
+            coord_x, coord_y = np.meshgrid(coord_x, coord_y)
+            coord = np.array([coord_x.flatten(), coord_y.flatten()])
+            coord_z = quadric_surface(coord, hess_original[ch, :, :], grad_original[ch, :], constant=cost[cost.shape[0]//2, cost.shape[1]//2, ch] if constants_original is None else constants_original[ch])
+            coord_z = coord_z.reshape(coord_x.shape)
+            if offset is not None:
+                coord_x += offset[0]
+                coord_y += offset[1]
+            _surf = ax.plot_surface(
+                coord_x, coord_y, coord_z,
+                cmap=cm.viridis,
+                linewidth=5, antialiased=False, alpha=0.3
+            )
+            ax.set_title("{}  -vs-  {}\n{}        {}\ngrad={}  -vs-  {}".format(hess_original[ch, 0, :], hess[ch, 0, :], hess_original[ch, 1, :], hess[ch, 1, :], grad_original[ch, :], grad[ch, :]))
+        else:
+            ax.set_title("{}\n{}\ngrad={}".format(hess[ch, 0, :], hess[ch, 1, :], grad[ch, :]))
+        # ax = fig.add_subplot(n_channels, 2, ch*2+2, projection='3d')
+
+    plt.show()
+
+
+def search_minimum(single_cost, debug=False, mode=QUADRATIC_FORM):
+    if mode == QUADRATIC_FORM:
+        hessi, gradi, constants = quadric_approximation(single_cost)
+        logging.info("GRADIENT QUADRATRIC FORM APPROACH {}".format(gradi))
+        logging.info("HESSIAN QUADRATRIC FORM APPROACH {}".format(hessi))
+    elif mode == SOBEL_DERIVATIVES:
+        hess, gradients, constants = derivatives(single_cost)
+        gradi = gradients[1, 1, :, :]
+        hessi = hess[0, 0, :, :, :]
+        logging.info("GRADIENT SOBEL APPROACH", gradi)
+        logging.info("HESSIAN SOBEL APPROACH", hessi)
+        if debug:
+            ch = 0
+            pr.show(
+                [
+                    [single_cost[:, :, ch]],
+                    [(gradients[:, :, ch, 0], "grad X"), (gradients[:, :, ch,  1], "grad Y")],
+                    [(hess[:, :, ch, 0, 0], "xx"), (hess[:, :, ch, 0, 1], "xy")],
+                    [(hess[:, :, ch, 1, 0], "yx"), (hess[:, :, ch, 1, 1], "yy")]
+                ]
+            )
     new_val = newton_iter(
         np.array([0., 0.]).T,
-        gradients[1, 1, :, :],
-        hess_mat = hess[0, 0, :, :, :]
+        gradi,
+        hess_mat = hessi,
+        max_step=None
     )
+    if debug:
+        quadratic_approximation_plot(single_cost, gradi, hessi)
     if debug:
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         coord_y = np.arange(-single_cost.shape[0]//2+1, single_cost.shape[0]//2+1, 1.)
         coord_x = np.arange(-single_cost.shape[1]//2+1, single_cost.shape[1]//2+1, 1.)
         coord_x, coord_y = np.meshgrid(coord_x, coord_y)
         coord_z = single_cost[:, :, 0].T
-        surf = ax.plot_surface(coord_x, coord_y, coord_z, cmap=cm.coolwarm,
-                               linewidth=0, antialiased=False)
-        ax.plot(new_val[1], new_val[0], "r.")
-        ch = 0
-        pr.show(
-            [
-                [single_cost[:, :, ch]],
-                [(gradients[:, :, ch, 0], "grad X"), (gradients[:, :, ch,  1], "grad Y")],
-                [(hess[:, :, ch, 0, 0], "xx"), (hess[:, :, ch, 0, 1], "xy")],
-                [(hess[:, :, ch, 1, 0], "yx"), (hess[:, :, ch, 1, 1], "yy")]
-            ]
+        surf = ax.plot_surface(
+            coord_x, coord_y, coord_z, cmap=cm.coolwarm,
+            linewidth=0, antialiased=False
         )
+        ax.plot(new_val[1], new_val[0], "r.")
+        plt.show()
+
     return new_val
 
 
 def search_full():
     single_cost = generate_synthetic_surface(size=128, center=[15.5, -30.], ch_number=1)
     start_point = [-40, -50]
-    res = search_minimum_full_patch_discrete(single_cost, iter=25, alpha=0.4, init_val=start_point, mode=NEWTON)
+    #CONVERGE IN A SINGLE STEP!
+    res = search_minimum_full_patch_discrete(single_cost, iter=25, alpha=1., init_val=start_point, mode=NEWTON, max_step=None)
+    plot_search(*res)
+    #CONVERGE IN A FEW STEPS DUE TO STEP CLIPPING
+    res = search_minimum_full_patch_discrete(single_cost, iter=25, alpha=1., init_val=start_point, mode=NEWTON, max_step=10.)
     plot_search(*res)
     res = search_minimum_full_patch_discrete(single_cost, iter=25, alpha=0.1, init_val=start_point, mode=GRADIENT_DESCENT)
     plot_search(*res)
@@ -121,7 +237,7 @@ def search_full():
 def search_multi_channel():
     single_cost = generate_synthetic_surface_multi(size=128, center=[15.5, -8.2])
     start_point = [-40, -50]
-    res = search_minimum_full_patch_discrete(single_cost, iter=100, alpha=0.2, init_val=start_point, mode=NEWTON)
+    res = search_minimum_full_patch_discrete(single_cost, iter=100, alpha=1., init_val=start_point, mode=NEWTON, max_step=10)
     plot_search(*res)
     res = search_minimum_full_patch_discrete(single_cost, iter=25, alpha=0.1, init_val=start_point, mode=GRADIENT_DESCENT)
     plot_search(*res)
@@ -129,32 +245,47 @@ def search_multi_channel():
     plot_search(*res)
 
 
-def real_search(cost_file=osp.join(osp.dirname(__file__), ".." , "samples" , "cost.npy")):
+def real_search(cost_file=osp.join(osp.dirname(__file__), "..", "samples" , "cost.npy")):
     costs = np.load(cost_file)
     center_y, center_x = costs.shape[2]//2, costs.shape[3]//2
     extraction_area = 5
     for i in range(costs.shape[0]):
         for j in range(costs.shape[1]):
-            print("patch {} {}".format(i, j))
+            logging.info("patch {} {}".format(i, j))
             single_cost = costs[
                           i, j,
                           center_y-extraction_area:center_y+extraction_area+1,
                           center_x-extraction_area:center_x+extraction_area+1,
                           :
                           ]
-            res = search_minimum_full_patch_discrete(single_cost, init_val = [0., 0.], alpha=0.8, iter=20, mode=NEWTON)
+            res = search_minimum_full_patch_discrete(
+                single_cost,
+                init_val = [0., 0.],
+                # init_val = [1., 2.],
+                # init_val = [2.25, -2.4], # CLOSE TO THE EDGE!
+                alpha=1.,
+                iter=5,
+                mode=NEWTON
+            )
             plot_search(*res)
 
 
 def test_search(debug=False):
     center=[0.8, -0.9]
-    single_cost = generate_synthetic_surface(size=5, center=center)
+    single_cost = generate_synthetic_surface(size=5, center=center, grad=[0., 0.] if not debug else [0.1, 0.5])
     estimated_center = search_minimum(single_cost, debug=debug)
-    assert (np.fabs(center - estimated_center)< 1E-8).all(), "{} vs {}".format(center, estimated_center)
+    if not debug:
+        assert (np.fabs(center - estimated_center)< 1E-8).all(), "{} vs {}".format(center, estimated_center)
+    else:
+        logging.info("Single parabola minimum search OK")
 
 
 if __name__ == "__main__":
-    test_search(debug=True)
-    real_search()
-    search_full()
+    log = logging.getLogger()
+    log.setLevel(logging.INFO)
     search_multi_channel()
+    test_search(debug=False)
+    test_search(debug=True)
+    search_full()
+    real_search()
+
