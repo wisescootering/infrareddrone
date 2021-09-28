8bd90cc4db64cebc14a1f06b95a9160d1e317bad
Balthazar <wisescootering@gmail.com>
2021-09-29 00:49:40 +0200

    [ALIGNMENT] refactoring, cost computation does not  involve any downsample or representatiion computation

--- registration/cost.py
+++ registration/cost.py
@@ -8,8 +8,74 @@ import irdrone.process as pr
 from registration.constants import LAPLACIAN_ENERGIES, GRAY_SCALE, COLORED, SSD, NTG
 
 
-def multispectral_representation(img, sigma_gaussian=5., mode=LAPLACIAN_ENERGIES):
-    """Laplacian Energy
+class AlignmentConfig:
+    def __init__(self, mode=LAPLACIAN_ENERGIES, dist_mode=NTG, downscale=1, sigma_ref=None, sigma_mov=None, num_patches = 5, search_size=6):
+        self.mode = mode
+        self.dist_mode = dist_mode
+        self.downscale = downscale
+        self.sigma_ref = sigma_ref
+        self.sigma_mov = sigma_mov
+        self.search_size = search_size
+        self.num_patches = num_patches
+        self.y_n = num_patches
+        self.x_n = num_patches
+        self.search_x = search_size
+        self.search_y = search_size
+
+# --------------------------------------------------- Helpers ----------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+def g2ci(ms_img):
+    avg = np.average(ms_img)
+    return g2c((255*(ms_img*0.5/avg).clip(0, 1)).astype(np.uint8))
+
+
+def viz_laplacian_energy(ms_img):
+    avg = np.average(ms_img)
+    out = (255*(ms_img[:, :, :3]*0.5/avg).clip(0, 1)).astype(np.uint8)
+    return out
+
+
+def debug_image_inputs(ref_orig, mov_orig, ref, mov, debug_fig=None, title="", mode=LAPLACIAN_ENERGIES):
+    """
+    Support various modes of plotting the images depending on the representation mode
+    """
+    debug_fig = None if debug_fig is None else (debug_fig + "inputs_"+mode+".png")
+    if mode == LAPLACIAN_ENERGIES:
+        pr.show(
+            [
+                [(ref_orig.data, "VIS"), (g2ci(ref[:, :, 0]), "-"), (g2ci(ref[:, :, 1]), "|"),
+                 (g2ci(ref[:, :, 2]), "/"), (g2ci(ref[:, :, 3]), "\\"), (viz_laplacian_energy(ref), "Energies")
+                 ],
+                [(mov_orig.data, "NIR"), (g2ci(mov[:, :, 0]), "-"), (g2ci(mov[:, :, 1]), "|"),
+                 (g2ci(mov[:, :, 2]), "/"), (g2ci(mov[:, :, 3]), "\\"), (viz_laplacian_energy(mov), "Energies")
+                 ]
+            ],
+            suptitle=title+"Directional energies {}".format(mov.shape),
+            save=debug_fig
+        )
+    if mode == GRAY_SCALE:
+        pr.show(
+            [
+                [(ref_orig.data, "VIS"), (g2ci(ref[:, :, 0]), "Gray")],
+                [(mov_orig.data, "NIR"), (g2ci(mov[:, :, 0]), "Gray")]
+            ],
+            suptitle=title+"Gray {}".format(mov.shape),
+            save=debug_fig
+        )
+    if mode == COLORED:
+        pr.show(
+            [
+                [(ref_orig.data, "VIS"), (mov_orig.data, "NIR")],
+            ],
+            suptitle=title+"Colored {}".format(mov.shape),
+            save=debug_fig
+        )
+
+
+# --------------------------------------------------- Representations --------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+def multispectral_representation(img, sigma_gaussian=None, mode=LAPLACIAN_ENERGIES):
+    """Laplacian Energy , Gray scales, Colors
     """
     shp = img.shape
     if mode == LAPLACIAN_ENERGIES:
@@ -22,14 +88,17 @@ def multispectral_representation(img, sigma_gaussian=5., mode=LAPLACIAN_ENERGIES
         ms_img[:, :, 2] = filters.edges.roberts_pos_diag(img_filt)
         ms_img[:, :, 3] = filters.edges.roberts_neg_diag(img_filt)
         ms_img = ms_img**2
-    if mode == GRAY_SCALE:
+    elif mode == GRAY_SCALE:
         ms_img = np.empty((shp[0], shp[1], 1))
         ms_img[:, :, 0] = 0.299*img[:, :, 0] + 0.587*img[:, :, 1] + 0.114*img[:, :, 2]
-    if mode == COLORED:
+    elif mode == COLORED:
         ms_img = img
+    else:
+        raise NameError("Representation {} is not supported".format(mode))
     return ms_img
 
-
+# ---------------------------------------------- Cost functions / distances --------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 def get_patch(ref, mov, crop):
     y_start, y_end, x_start, x_end = crop
     patch_ref = ref[y_start:y_end, x_start:x_end, :]
@@ -50,8 +119,8 @@ def cost_surface_SSD(patch_ref, patch_mov_search, search_y, search_x):
     return cost
 
 
-# @jit(nopython=True)
-def cost_surface_NCC(patch_ref, patch_mov_search, search_y, search_x):
+@jit(nopython=True)
+def cost_surface_NTG(patch_ref, patch_mov_search, search_y, search_x):
     """Normalized Correlation Coefficient - optimized by Numba
     """
     pm_s_y, pm_s_x, c_s = patch_mov_search.shape
@@ -68,18 +137,14 @@ def cost_surface_NCC(patch_ref, patch_mov_search, search_y, search_x):
     return cost
 
 
-def g2ci(ms_img):
-    avg = np.average(ms_img)
-    return g2c((255*(ms_img*0.5/avg).clip(0, 1)).astype(np.uint8))
-
-
-def viz_laplacian_energy(ms_img):
-    avg = np.average(ms_img)
-    out = (255*(ms_img[:, :, :3]*0.5/avg).clip(0, 1)).astype(np.uint8)
-    return out
-
-
-def compute_surfaces(ref, mov, debug=False, y_n=3, x_n=3, search_y=3, search_x=3, debug_fig=None, mode=LAPLACIAN_ENERGIES, dist_mode=SSD, suffix="", prefix=""):
+# ---------------------------------------------- Full image cost function ----------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+def compute_cost_surfaces(
+        ref, mov,
+        y_n=3, x_n=3, search_y=3, search_x=3,
+        debug=False, debug_fig=None, mode=LAPLACIAN_ENERGIES, dist_mode=SSD, downscale=1, suffix="", prefix=""
+    ):
+    # @TODO: remove debug figures, could be numba only
     y_s, x_s, c_s = ref.shape
     p_size_x = int(np.floor(x_s / x_n))
     p_size_y = int(np.floor(y_s / y_n))
@@ -98,8 +163,8 @@ def compute_surfaces(ref, mov, debug=False, y_n=3, x_n=3, search_y=3, search_x=3
             patch_mov_search = patch_mov[search_y:-search_y, search_x:-search_x, :]
             if dist_mode == SSD:
                 cost = cost_surface_SSD(patch_ref.astype(np.float32), patch_mov_search.astype(np.float32), search_y, search_x)
-            else:
-                cost = cost_surface_NCC(patch_ref.astype(np.float32), patch_mov_search.astype(np.float32), search_y, search_x)
+            elif dist_mode == NTG:
+                cost = cost_surface_NTG(patch_ref.astype(np.float32), patch_mov_search.astype(np.float32), search_y, search_x)
             if debug or debug_fig is not None:
                 debug_fig_current = None
                 if debug_fig is not None:
@@ -139,104 +204,80 @@ def compute_surfaces(ref, mov, debug=False, y_n=3, x_n=3, search_y=3, search_x=3
                 )
             costs[y_id].append(cost)
             costs_dbg[y_id].append(np.sum(cost, axis=2))
-    return costs, costs_dbg, centers
-
-
-def debug_image_inputs(ref_orig, mov_orig, ref, mov, debug_fig=None, title="", mode=LAPLACIAN_ENERGIES):
-    """
-    - Laplacian Energies
-    """
-    debug_fig = None if debug_fig is None else (debug_fig + "inputs_"+mode+".png")
-    if mode == LAPLACIAN_ENERGIES:
-        pr.show(
-            [
-                [(ref_orig.data, "VIS"), (g2ci(ref[:, :, 0]), "-"), (g2ci(ref[:, :, 1]), "|"),
-                 (g2ci(ref[:, :, 2]), "/"), (g2ci(ref[:, :, 3]), "\\"), (viz_laplacian_energy(ref), "Energies")
-                 ],
-                [(mov_orig.data, "NIR"), (g2ci(mov[:, :, 0]), "-"), (g2ci(mov[:, :, 1]), "|"),
-                 (g2ci(mov[:, :, 2]), "/"), (g2ci(mov[:, :, 3]), "\\"), (viz_laplacian_energy(mov), "Energies")
-                 ]
-            ],
-            suptitle=title+"Directional energies {}".format(mov.shape),
-            save=debug_fig
-        )
-    if mode == GRAY_SCALE:
-        pr.show(
-            [
-                [(ref_orig.data, "VIS"), (g2ci(ref[:, :, 0]), "Gray")],
-                [(mov_orig.data, "NIR"), (g2ci(mov[:, :, 0]), "Gray")]
-            ],
-            suptitle=title+"Gray {}".format(mov.shape),
-            save=debug_fig
-        )
-    if mode == COLORED:
-        pr.show(
-            [
-                [(ref_orig.data, "VIS"), (mov_orig.data, "NIR")],
-            ],
-            suptitle=title+"Colored {}".format(mov.shape),
-            save=debug_fig
-        )
-
-
-def align_single_scale(ref, mov, downscale=1, debug=False, debug_dir=None, prefix=None, suffix="", y_n=5, x_n=5, search_y=5, search_x=5, mode=LAPLACIAN_ENERGIES, dist_mode=SSD):
-    if downscale >1:
-        ref_pyr = transform.pyramid_reduce(ref, downscale=downscale, multichannel=True)
-        mov_pyr = transform.pyramid_reduce(mov, downscale=downscale, multichannel=True)
-        logging.info("Downscale from {} to {}".format(ref.shape, ref_pyr.shape))
-        if prefix is None:
-            prefix = "ds_{}".format(downscale)
-    else:
-        ref_pyr, mov_pyr = ref, mov
-    if prefix is None:
-        prefix = ""
-    debug_fig_main = None if debug_dir is None else osp.join(debug_dir, "{}_blocks_y{}x{}_{}_{}_".format(prefix, y_n, x_n, mode, dist_mode))
-    costs, _costs_dbg, centers = compute_surfaces(
-        ref_pyr, mov_pyr, y_n=y_n, x_n=x_n, search_x=search_x, search_y=search_y,
-        debug=debug,
-        debug_fig=debug_fig_main, prefix=prefix, suffix=suffix,
-        mode=mode,
-        dist_mode=dist_mode,
-    )
-    debug_fig = None if debug_fig_main is None else debug_fig_main + "overview_cost_surfaces_{}.png".format(suffix)
     cost_dict = {
         "costs": np.array(costs),
         "centers": centers,
         "downscale": downscale
     }
+    return cost_dict, costs_dbg
+
+
+def compute_cost_surfaces_with_traces(
+        ref, mov,
+        debug=False, debug_dir=None, prefix=None, suffix="", align_config=AlignmentConfig()
+    ):
+    """Run compute_cost_surfaces with extra traces
+    @TODO: normally, debug figures shall be plotted here only
+    """
+    if prefix is None:
+        prefix = ""
+    debug_fig_main = None if debug_dir is None else osp.join(debug_dir, "{}_blocks_y{}x{}_search_y{}x{}_{}_".format(
+        prefix, align_config.y_n, align_config.x_n, align_config.search_y, align_config.search_x, align_config.dist_mode))
+    cost_dict, _costs_dbg = compute_cost_surfaces(
+        ref, mov,
+        y_n=align_config.y_n, x_n=align_config.x_n,
+        search_x=align_config.search_x, search_y=align_config.search_y,
+        debug=debug,
+        debug_fig=debug_fig_main, prefix=prefix, suffix=suffix,
+        mode=align_config.mode,
+        dist_mode=align_config.dist_mode,
+    )
+    debug_fig = None if debug_fig_main is None else debug_fig_main + "overview_cost_surfaces_{}.png".format(suffix)
+
     if debug or debug_dir is not None:
         pr.show(
-            _costs_dbg, suptitle="cost {} surfaces {}, ds {} y{} x{} {} {}".format(dist_mode, mode, downscale, y_n, x_n, prefix, suffix),
+            _costs_dbg, suptitle="cost {} surfaces y{} x{} {} {}".format(
+                align_config.dist_mode, align_config.y_n, align_config.x_n, prefix, suffix),
             save=debug_fig
         )
         np.save(debug_fig_main+"_cost_search_{}".format(suffix), cost_dict)
     return cost_dict
 
 
-def align(ref_orig, mov_orig, debug=False, debug_dir=None, mode=LAPLACIAN_ENERGIES, dist_mode=SSD, downscale=1, prefix=None, suffix="", sigma_ref=5., sigma_mov=3.):
-    # @TODO: move spectral representation outside align!
-    # @TODO: move downscale outside align!
-    ref = multispectral_representation(ref_orig if isinstance(ref_orig, np.ndarray) else ref_orig.data, sigma_gaussian=sigma_ref, mode=mode)
-    mov = multispectral_representation(mov_orig if isinstance(mov_orig, np.ndarray) else mov_orig.data, sigma_gaussian=sigma_mov, mode=mode) # reducing blur for NIR image
-    if prefix is None:
-        if downscale>1:
-            prefix = "ds_{}".format(downscale)
-        else:
-            prefix = ""
+def run_multispectral_cost(ref_orig, mov_orig, debug_dir=None, debug=True, suffix="", align_config=AlignmentConfig()):
+    """
+    - Switch to a specific multi-spectral representation
+    - Downscale
+    - Compute costs
+    """
+    if debug_dir is not None and not osp.isdir(debug_dir):
+        mkdir(debug_dir)
+    ref = multispectral_representation(
+        ref_orig if isinstance(ref_orig, np.ndarray) else ref_orig.data,
+        sigma_gaussian=align_config.sigma_ref, mode=align_config.mode
+    )
+    mov = multispectral_representation(
+        mov_orig if isinstance(mov_orig, np.ndarray) else mov_orig.data,
+        sigma_gaussian=align_config.sigma_mov, mode=align_config.mode
+    )
+    prefix = "ds{}_{}".format(align_config.downscale, align_config.mode)
     if debug or debug_dir is not None:
         debug_fig = None if debug_dir is None else osp.join(debug_dir, "{}_{}".format(prefix, suffix))
-        debug_image_inputs(ref_orig, mov_orig, ref, mov, debug_fig=debug_fig, title="{} {}".format(prefix, suffix), mode=mode)
-    num_patches = 5
-    search_size = 6
-    y_n = num_patches
-    x_n = num_patches
-    cost_dict = align_single_scale(
-        ref, mov, debug=debug, debug_dir=debug_dir, prefix=prefix, suffix=suffix,
-        downscale=downscale, y_n=y_n, x_n=x_n, search_y=search_size, search_x=search_size,
-        mode=mode,
-        dist_mode=dist_mode
+        debug_image_inputs(ref_orig, mov_orig, ref, mov, mode=align_config.mode, debug_fig=debug_fig, title="{} {}".format(prefix, suffix))
+    if align_config.downscale >1:
+        ref_pyr = transform.pyramid_reduce(ref, downscale=align_config.downscale, multichannel=True)
+        mov_pyr = transform.pyramid_reduce(mov, downscale=align_config.downscale, multichannel=True)
+        logging.info("Downscale from {} to {}".format(ref.shape, ref_pyr.shape))
+    else:
+        ref_pyr, mov_pyr = ref, mov
+
+    compute_cost_surfaces_with_traces(
+        ref_pyr, mov_pyr, debug_dir=debug_dir,
+        prefix=prefix,
+        debug=debug,
+        align_config=align_config
+
     )
-    return cost_dict
 
 
 if __name__ == '__main__':
@@ -245,12 +286,16 @@ if __name__ == '__main__':
     path = osp.join(osp.dirname(__file__), "..", "samples")
     nir_img = pr.Image(osp.join(path, "20210906123134_PL4_DIST_raw_NON_LINEAR_IR_registered_semi_auto.jpg"))
     vis_img = pr.Image(osp.join(path, "20210906123134_PL4_DIST_raw_NON_LINEAR_REF.jpg"))
-    debug_dir="_debug_cost"
+    debug_dir = "_debug_cost_new_checko"
     if not osp.isdir(debug_dir):
         mkdir(debug_dir)
-    align(
-        vis_img, nir_img, debug_dir=debug_dir,
+    align_conf = AlignmentConfig(
         mode=[LAPLACIAN_ENERGIES, GRAY_SCALE, COLORED][0],
         dist_mode=[SSD, NTG][1],
-        downscale=16
+        downscale=16,
+        sigma_ref=5.,
+        sigma_mov=3.,  # reducing blur for NIR image
+        num_patches=6,
+        search_size=5
     )
+    run_multispectral_cost(vis_img, nir_img, align_config=align_conf, debug_dir=debug_dir)
