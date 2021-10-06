40486a590d65cc18f8eff4925ab2cbdfce0d3856
Balthazar <wisescootering@gmail.com>
2021-10-06 23:16:19 +0200

    [AUTO ALIGNMENT] Manual pre-step option, command line option, VIR fusion, add .bat file writing to help debugging a specific file or simply manually redoing a failed case.

--- automatic_registration.py
+++ automatic_registration.py
@@ -5,7 +5,7 @@ import numpy as np
 import logging
 import os.path as osp
 import registration.rigid as rigid
-from irdrone.semi_auto_registration import manual_warp
+from irdrone.semi_auto_registration import manual_warp, ManualAlignment, Transparency, Absgrad
 from skimage import transform
 import os
 osp = os.path
@@ -16,6 +16,8 @@ from customApplication import colorMapNDVI
 import irdrone.imagepipe as ipipe
 import shutil
 import cv2
+import argparse
+
 
 class VegetationIndex(ipipe.ProcessBlock):
     """
@@ -32,7 +34,7 @@ class VegetationIndex(ipipe.ProcessBlock):
 def ndvi(vis_img, nir_img, out_path=None):
     ndvi = VegetationIndex("NDVI", vrange=[(-1., 1., 0)], inputs=[1, 2], outputs=[0,], )
     ipi = ipipe.ImagePipe(
-        [vis_img.data, nir_img.data],
+        [vis_img, nir_img],
         sliders=[ndvi],
         floatpipe=True
     )
@@ -48,7 +50,45 @@ def ndvi(vis_img, nir_img, out_path=None):
         ipi.save(out_path)
 
 
-def align_raw(vis_path, nir_path, cals, debug_dir=None, debug=False, extension=1.4):
+def vir(vis_img, nir_img, out_path=None):
+    vir = vis_img.copy()
+    vir[:, :, 0] = np.mean(nir_img, axis=-1)
+    vir[:, :, 1] = vis_img[:, :, 0]
+    vir[:, :, 2] = vis_img[:, :, 1]
+    if out_path is None:
+        pr.Image(vir).show()
+    else:
+        pr.Image(vir).save(out_path)
+
+def user_assisted_manual_alignment(ref, mov, cals):
+    ROTATE = "Rotate"
+    rotate3d = ManualAlignment(
+        ROTATE,
+        slidersName=["YAW [Horizontal]", "PITCH [Vertical]", "ROLL"],
+        inputs=[1, 2],
+        outputs=[0],
+        vrange=[
+            (-30., 30, 0.), (-30., 30, 0.), (-5., 5, 0.)
+        ]
+    )
+    rotate3d.set_movingcalib(cals["movingcalib"])
+    rotate3d.set_refcalib(cals["refcalib"])
+    alpha = Transparency("Alpha", inputs=[0, 1], vrange=(-1., 1., -0.45))
+    absgrad_viz = Absgrad("Absgrad visualize", inputs=[0, 1], outputs=[0, 1], vrange=(0, 1, 1))
+    ipi = ipipe.ImagePipe(
+        [ref, mov],
+        rescale=None,
+        sliders=[
+            rotate3d,
+            absgrad_viz,
+            alpha
+        ]
+    )
+    ipi.gui()
+    return rotate3d.alignment_parameters
+
+
+def align_raw(vis_path, nir_path, cals, debug_dir=None, debug=False, extension=1.4, manual=True):
     """
     :param vis_path: Path to visible DJI DNG image
     :param nir_path: Path to NIR SJCAM M20 RAW image
@@ -76,19 +116,23 @@ def align_raw(vis_path, nir_path, cals, debug_dir=None, debug=False, extension=1
     mov_full = nir.data
     ts_end_load = time.perf_counter()
     logging.warning("{:.2f}s elapsed in loading full resolution RAW".format(ts_end_load - ts_start))
+    if manual:
+        alignment_params = user_assisted_manual_alignment(ref_full, mov_full, cals)
+        yaw_main, pitch_main, roll_main = alignment_params["yaw"], alignment_params["pitch"], alignment_params["roll"]
+    else:
+        yaw_main, pitch_main, roll_main = 0., 0., 0.
     ts_start_coarse_search = time.perf_counter()
     ds = 32
     # -------------------------------------------------------------- Full res : Undistort NIR fisheye with a larger FOV
-    yaw_main, pitch_main, roll_main = 0., 0., 0.
     mov_w_full = manual_warp(
         ref_full, mov_full,
         yaw_main, pitch_main, roll_main,
         refcalib=cals_ref, movingcalib=cals["movingcalib"],
         refinement_homography=None,
         bigger_size_factor=extension,
-
     )
-    # -------------------------------------------------------------- Multi-spectral representation
+    # ------------------------------------------------------------------------------------ Multi-spectral representation
+    # @TODO : REFACTOR! This is exactly the same kind of data preparation for pyramidal search
     msr_mode = rigid.LAPLACIAN_ENERGIES
     # @TODO : Fix Gaussian
     msr_ref_full = rigid.multispectral_representation(ref_full, mode=msr_mode, sigma_gaussian=3.).astype(np.float32)
@@ -122,7 +166,9 @@ def align_raw(vis_path, nir_path, cals, debug_dir=None, debug=False, extension=1
     yaw_refine = np.rad2deg(np.arctan(translation[0]/focal))
     pitch_refine = np.rad2deg(np.arctan(translation[1]/focal))
     print("2D translation {}".format(translation, yaw_refine, pitch_refine))
-
+    ts_end_coarse_search = time.perf_counter()
+    logging.warning("{:.2f}s elapsed in coarse search".format(ts_end_coarse_search - ts_start_coarse_search))
+    ts_start_warp = time.perf_counter()
     if debug:
         mov_wr = manual_warp(
             msr_ref, cv2.resize(mov_full, (mov_full.shape[1]//ds, mov_full.shape[0]//ds)),
@@ -139,8 +185,8 @@ def align_raw(vis_path, nir_path, cals, debug_dir=None, debug=False, extension=1
         refcalib=cals_ref, movingcalib=cals["movingcalib"],
         geometric_scale=None, refinement_homography=None,
     )
-    ts_end_coarse_search = time.perf_counter()
-    logging.warning("{:.2f}s elapsed in coarse search".format(ts_end_coarse_search - ts_start_coarse_search))
+    ts_end_warp = time.perf_counter()
+    logging.warning("{:.2f}s elapsed in warping from coarse search".format(ts_end_warp - ts_start_warp))
     pr.Image(ref_full).save(osp.join(debug_dir, "FULLRES_REF.jpg"))
     pr.Image(mov_wr_fullres).save(osp.join(debug_dir, "FULLRES_REGISTERED_COARSE.jpg"))
     mov_w_final = rigid.pyramidal_search(
@@ -156,55 +202,39 @@ def align_raw(vis_path, nir_path, cals, debug_dir=None, debug=False, extension=1
     # pr.show([ref, mov, mov_w], suptitle="{}".format(mov.shape))
     ts_end = time.perf_counter()
     logging.warning("{:.2f}s elapsed in total alignment".format(ts_end - ts_start))
+    return ref_full, mov_w_final
 
 
-def demo_raw(folder=osp.join(osp.dirname(__file__), r"Hyperlapse 06_09_2021_sync")):
+def process_raw_folder(folder, delta=timedelta(seconds=171), manual=False):
+    """NIR/VIS image alignment and fusion
+    - using a simple synchronization mechanism based on exif and camera deltas
+    - camera time delta
     """
-    Scan the folder for pairs of raw with the same name.
-    Assume that RAW from SJCAM M20 and DNG from DJI have the same file name
-    """
-    data_link = "https://drive.google.com/drive/folders/1IrF55tDYV6YwHm0gUTRc7T3WTnbwbPxk?usp=sharing"
-    assert osp.isdir(folder), "please download {} and put all images in {}".format(data_link, folder)
-    cals = dict(
-        refcalib=ut.cameracalibration(camera="DJI_RAW"),
-        movingcalib=ut.cameracalibration(camera="M20_RAW")
-    )
-    out_dir = osp.join(folder, "debug")
-    if not osp.isdir(out_dir):
-        import os
-        os.mkdir(out_dir)
-    import glob
-    for vis_pth in sorted(glob.glob(osp.join(folder, "*DNG"))):
-        nir_pth = vis_pth.replace("DNG", "RAW")
-        # @TODO: FIX WHEN DJI GEOMETRIC CALIBRATION IS FIXED
-        vis_pth_undist = vis_pth.replace(".DNG", "_PL4_DIST.tif")
-        if osp.exists(vis_pth_undist):
-            vis_pth = vis_pth_undist
-        align_raw(vis_pth, nir_pth, cals, debug_dir=osp.basename(vis_pth)[:-4])
-
-
-def process_raw_folder(folder, delta=timedelta(seconds=171)):
-    """Synchronize image pairs"""
     sync_pairs = synchronize_data(folder, replace_dji=(".DNG", "_PL4_DIST.tif"), delta=delta)
     cals = dict(refcalib=ut.cameracalibration(camera="DJI_RAW"), movingcalib=ut.cameracalibration(camera="M20_RAW"))
     out_dir = osp.join(folder, "_RESULTS")
-    process_raw_pairs(sync_pairs, cals, folder=folder, out_dir=out_dir)
+    process_raw_pairs(sync_pairs, cals, folder=folder, out_dir=out_dir, manual=manual)
 
 
 def process_raw_pairs(
         sync_pairs,
         cals=dict(refcalib=ut.cameracalibration(camera="DJI_RAW"), movingcalib=ut.cameracalibration(camera="M20_RAW")),
-        folder=None, out_dir=None):
+        folder=None, out_dir=None, manual=False, debug=False
+    ):
     if folder is None:
         folder = osp.dirname(sync_pairs[0][0])
     if out_dir is None:
         out_dir = osp.join(folder, "_RESULTS")
     if not osp.exists(out_dir):
         os.mkdir(out_dir)
-    for vis_pth, nir_pth in sync_pairs:  # [::-1]  [::-1][4:5]:  [3::40]: [410:415]:
+    for vis_pth, nir_pth in sync_pairs[::-1]:  # [::-1]  [::-1][4:5]:  [3::40]: [410:415]:
         logging.warning("processing {} {}".format(osp.basename(vis_pth), osp.basename(nir_pth)))
-        debug_dir = osp.join(folder, osp.basename(vis_pth)[:-4]+"_align_debuggin")
-        align_raw(vis_pth, nir_pth, cals, debug_dir=debug_dir)
+        debug_dir = osp.join(folder, osp.basename(vis_pth)[:-4]+"_align_GLOBAL_MIN_FIXED" + ("_manual" if manual else ""))
+        write_manual_bat_redo(vis_pth, nir_pth, osp.join(out_dir, osp.basename(vis_pth[:-4])+"_REDO.bat"), debug=False)
+        write_manual_bat_redo(vis_pth, nir_pth, osp.join(out_dir, osp.basename(vis_pth[:-4])+"_DEBUG.bat"), debug=True)
+        ref_full, aligned_full = None, None
+        if not osp.isdir(debug_dir):
+            ref_full, aligned_full = align_raw(vis_pth, nir_pth, cals, debug_dir=debug_dir, manual=manual, debug=debug)
         if osp.isdir(debug_dir):
             ref_pth = osp.join(debug_dir, "FULLRES_REF.jpg")
             aligned_pth = osp.join(debug_dir, "FLOW_it09_ds04_WARP_LOCAL.jpg")
@@ -221,73 +251,54 @@ def process_raw_pairs(
                 shutil.copy(search_pth, osp.join(out_dir, "_GLOBAL_SEARCH_" + osp.basename(vis_pth[:-4])+"_Global_cost.png"))
                 local_cost_pth = osp.join(debug_dir, "ds4_laplacian_energies_blocks_y5x5_search_y6x6_NTG_overview_cost_surfaces__it09.png")
                 shutil.copy(local_cost_pth, osp.join(out_dir, "_LOCAL_COSTS_" + osp.basename(vis_pth[:-4])+"_Global_cost.png"))
-            ndvi(
-                pr.Image(ref_pth),
-                pr.Image(aligned_pth),
-                out_path=osp.join(out_dir, "_NDVI_" + osp.basename(vis_pth[:-4])+".jpg")
-            )
+            if ref_full is None :
+                ref_full = pr.Image(ref_pth).data
+            if aligned_full is None:
+                aligned_full = pr.Image(aligned_pth).data
+            ndvi(ref_full, aligned_full, out_path=osp.join(out_dir, "_NDVI_" + osp.basename(vis_pth[:-4])+".jpg"))
+            vir(ref_full, aligned_full, out_path=osp.join(out_dir, "_VIR_" + osp.basename(vis_pth[:-4])+".jpg"))
 
 
-def demo_raw_real_application(folder_database=osp.join(osp.dirname(__file__), "FlightDatabase")):
-    data_link = "https://drive.google.com/drive/folders/1UJGvq8gWpkkgtAd6cRbNiQIRyabcjPIf?usp=sharing"
-    assert osp.isdir(folder_database), "please download {} and put all images in {}".format(data_link, folder_database)
-    cals = dict(
-        refcalib=ut.cameracalibration(camera="DJI_RAW"),
-        movingcalib=ut.cameracalibration(camera="M20_RAW")
-    )
-    # -------------------------------------------------------------------------------------------------------- Visargent
+def write_manual_bat_redo(vis_pth, nir_pth, debug_bat_pth, out_dir=None, debug=False):
+    if out_dir is None:
+        out_dir = osp.abspath(debug_bat_pth.replace(".bat", ""))
+    with open(debug_bat_pth, "w") as fi:
+        fi.write("call activate {}\n".format(os.environ['CONDA_DEFAULT_ENV']))
+        fi.write("python {} --images {} {} --manual --outdir {} {}\n".format(
+            osp.abspath(__file__),
+            vis_pth, nir_pth,
+            out_dir,
+            "--debug" if debug else "")
+        )
+        fi.write("call deactivate\n")
 
-    img_pairs = [
-        ["DJI_0612.DNG", "2021_0505_083056_111.RAW"],
-        ["DJI_0613.DNG", "2021_0505_083132_135.RAW"],
-        ["DJI_0615.DNG", "2021_0505_083217_165.RAW"]
-    ]
-    folder = osp.join(folder_database, "FLY-20210505_Visargent", "AerialPhotography")
-    pairs_paths = [[osp.join(folder, vis.replace(".RAW", "_PL4_DIST.tif")), osp.join(folder, nir)]
-                   for vis, nir in img_pairs]
-    batch_raw_alignment(folder, pairs_paths, cals)
-
-    # ----------------------------------------------------------------------------------------------------------- Vivans
-    img_pairs = [
-        ["DJI_0618.DNG", "2021_0505_130901_067.RAW"],  # NOT WORKING IN AUTO! -> NEEDS MANUAL
-        # ["DJI_0623.DNG", "2021_0505_131131_167.RAW"],
-        # ["DJI_0624.DNG", "2021_0505_131155_183.RAW"]
-        # ["DJI_0624.DNG", "2021_0505_131131_167.RAW"]
-    ]
-    folder = osp.join(folder_database, "FLY-20210505_Vivans", "AerialPhotography")
-    pairs_paths = [[osp.join(folder, vis.replace(".RAW", "_PL4_DIST.tif")), osp.join(folder, nir)]
-                   for vis, nir in img_pairs]
-    batch_raw_alignment(folder, pairs_paths, cals, manual=True)
-
-
-def batch_raw_alignment(folder, pairs_paths, cals, manual=False):
-    """allows manual alignment for the first part
-    """
-    out_dir = osp.join(folder, "debug")
-    if not osp.isdir(out_dir):
-        os.mkdir(out_dir)
-    for vis_pth, nir_pth in pairs_paths:
-        dbg_dir = osp.join(out_dir, osp.basename(vis_pth)[:-4])
-        if not osp.isdir(dbg_dir):
-            os.mkdir(dbg_dir)
-        if manual:
-            from irdrone.semi_auto_registration import align_raw as align_raw_manual
-            out_lin, out_non_lin, ref_full = align_raw_manual(vis_pth, nir_pth, cals, out_dir=dbg_dir+"_manual")
-            mov_w_final = rigid.pyramidal_search(
-                ref_full, out_non_lin,
-                iterative_scheme=[(16, 2, 4), (16, 2, 5), (4, 3, 5)],
-                mode=rigid.LAPLACIAN_ENERGIES, dist=rigid.NTG,
-                debug=False, debug_dir=dbg_dir,
-                affinity=False,
-                sigma_ref=5.,
-                sigma_mov=3.
-            )
-            pr.Image(mov_w_final).save(osp.join(dbg_dir, "FULLRES_REGISTERED_REFINED.jpg"))
-        else:
-            align_raw(vis_pth, nir_pth, cals, debug_dir=dbg_dir, extension=1.4)
 
 
 if __name__ == "__main__":
-    # demo_raw()
-    process_raw_folder(folder = r"D:\FLY-20210906-Blassac-05ms\AerialPhotography")
-    # demo_raw_real_application()
+    parser = argparse.ArgumentParser(description='Align images')
+    parser.add_argument('--images', nargs='+', help='list of images')
+    parser.add_argument('--manual', action="store_true", help='manual alignement')
+    parser.add_argument('--outdir', help='output directory')
+    parser.add_argument('--debug', action="store_true", help='full debug traces')
+    args = parser.parse_args()
+    print(args.manual)
+    if args.images is None:
+        process_raw_folder(folder = r"D:\FLY-20210906-Blassac-05ms\AerialPhotography", manual=args.manual)
+    else:
+        im_pair = args.images
+        if im_pair[0].lower().endswith(".raw"):
+            im_pair = im_pair[::-1]
+        assert im_pair[0].lower().endswith(".tif") or im_pair[0].lower().endswith(".dng"), "Drone image first, SJcam image second"
+        im_pairs = [im_pair]
+        if args.outdir is not None:
+            out_dir = args.outdir
+        else:
+            out_dir = osp.dirname(im_pairs[0][0])
+        process_raw_pairs(
+            im_pairs,
+            folder=out_dir,
+            out_dir=out_dir,
+            manual=args.manual,
+            debug=args.debug,
+        )
+
